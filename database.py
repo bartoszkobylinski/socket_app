37ad75373e8f2dcae05738cee6bc4527795cf7c8
Bartosz Kobylinski <bartoszkobylinski@Bartoszs-Mac-mini.local>
2022-06-01 23:34:46 +0200

    working with methods to get access to create/edit/updating/checking records in json file as database for project

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 
+import json
+
+
 def create_databse():
     with open('database.json', "w") as db:
-        pass
+        db.write(json.dumps({'users':[]}))
 
 def check_database(data):
     with open ('database.json', 'r') as db:
@@ -10,11 +13,18 @@ def check_database(data):
 def add_to_database(data):
     with open ('database.json', 'w') as db:
         pass
-
+        
 def remove_from_database(data):
-    with open('database', 'w') as db:
+    with open('database', 'w') as db: 
         pass
 
 def update_record_in_databe(data):
     with open('database', 'w') as db:
         pass
+
+{
+    'users': [
+        {'name':'bartek','email':'kal@wp.pl','password':'jOKotas324',
+        'admin':True, 'logged':False, "messages":['oeuoe','oeuoeu','oeuoeu']}
+    ]
+}
