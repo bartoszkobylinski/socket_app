2eec14d2b4bd8dc839ab3ab456c4da716e254068
Bartosz Kobylinski <bartoszkobylinski@Bartoszs-Mac-mini.local>
2022-08-24 08:21:33 +0200

    Adding more testing cases and modyfing function in database.py

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ def create_databse(name):
         db.write(json.dumps({'users':[]}))
 
 def check_if_user_exists_in_database(username):
-    with open ('database.json', 'r') as db:
+    with open ('test_database.json', 'r') as db:
         json_database = json.load(db)
     for usersname in json_database.get("users",''):
         if username in usersname.get("user",''):
@@ -17,7 +17,7 @@ def check_if_user_exists_in_database(username):
     
 def read_user_mailbox(username):
     mailbox_to_show = list()
-    with open('database.json','r') as db:
+    with open('test_database.json','r') as db:
         json_database = json.load(db) 
         for user in json_database.get("users",''):
             if username == user.get("user",''):
@@ -25,34 +25,42 @@ def read_user_mailbox(username):
                 mailbox_to_show = unread_mailbox
                 user.get("mailbox").extend(unread_mailbox)
                 user["unread_mailbox"] = list()
-        with open("database.json", 'w') as db:
+        with open("test_database.json", 'w') as db:
             json.dump(json_database, db)  
     return mailbox_to_show
 
 def read_all_user_mailbox():
-    with open ('database.json','r') as db:
+    with open ('test_database.json','r') as db:
         all_unread_mailbox = list()
         json_database = json.load(db)
+        print(f"to jest baza danych po zaladowaniu: {json_database}")
         for user in json_database.get("users",''):
+            print(f"jestesmy na takim user: {user}")
             unread_mailbox = user.get("unread_mailbox")
-            all_unread_mailbox.append({'user':user, 'unread_mailbox': unread_mailbox})
+            print(f"to est {user.get('unread_mailbox')}")
+            all_unread_mailbox.append({'user':user.get("user",''), 'unread_mailbox': unread_mailbox})
+            print(f"to jest w all_unread_mailbox: {all_unread_mailbox}")
+            print(f"to jest dlugosc {len(all_unread_mailbox)}")
             user.get("mailbox").extend(unread_mailbox)
             user['unread_mailbox'] = list()
-        with open("database.json", 'w') as db:
+        with open("test_database.json", 'w') as db:
             json.dump(json_database, db) 
+        for user in all_unread_mailbox:
+            print("-------")
+            print(user)
     return all_unread_mailbox
 
 
 
 def add_user_to_database(user, password):
-    with open ('database.json', 'r') as db:
+    with open ('test_database.json', 'r') as db:
         json_database = json.load(db)
     if len(json_database.get('users','')) < 1:
         json_database['users'].append({'user': user, 'password':password, "mailbox": list(), 'unread_mailbox':list(), 'admin': True})
     else:
         json_database['users'].append({'user': user, 'password':password, 'mailbox': list(), 'unread_mailbox':list(), 'admin': False})
     
-    with open("database.json", "w") as db:
+    with open("test_database.json", "w") as db:
         json.dump(json_database, db)        
 
 def change_password_in_user_database(username, password):
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 
+import json
 import unittest
 
 from user import User
+from database import check_if_user_exists_in_database, create_databse, add_user_to_database, read_all_user_mailbox, read_user_mailbox
+import os
+
+
+# User file
 
 class TestUser(unittest.TestCase):
 
@@ -15,6 +21,49 @@ class TestUser(unittest.TestCase):
         self.assertEqual(str(self.user), 'bartek logged: False admin: False')
 
 
+# database file
 
+class TestDatabase(unittest.TestCase):
+    
+    def test_if_database_is_created(self):
+        create_databse("test_database.json")
+        path_to_database = '/Users/bartoszkobylinski/Programming/Python/socket_server/test_database.json'
+        self.assertTrue(os.path.exists(path_to_database))
+
+    def test_if_user_is_added_to_database(self):
+        user = 'bartek'
+        password = 'halina'
+        add_user_to_database(user=user, password=password)
+        self.assertEqual(check_if_user_exists_in_database(username=user), True)
+     
+    def test_if_user_exists_in_database(self):
+        self.assertEqual(True, check_if_user_exists_in_database(username = 'bartek'))
+    
+    def test_read_user_mailbox(self):
+    
+        with open ('test_database.json','r') as db:
+            test_json_database = json.load(db)
+            read_mailbox = 1
+            for user in test_json_database.get('users',''):
+                if user.get('user','') == 'bartek':
+                    user.get("unread_mailbox").append("halina, hahlina")      
+            with open('test_database.json','w') as db:
+                json.dump(test_json_database, db)
+        read_user_mailbox('bartek')
+        with open('test_database.json', 'r') as db:
+            test_json_database = json.load(db)
+            self.assertEqual(1, len(test_json_database['users'][0]['mailbox']))
+    
+    def test_read_all_user_mailbox(self):
+        mails = read_all_user_mailbox()
+        print(f"to jest mails: {mails}")
+        mails_quantity = 0 
+        for user in mails:
+            mails_quantity += int(len(user.get("unread_mailbox",'')))
+            print(f"mail_quantity: {mails_quantity}")
+
+        self.assertEqual(4, mails_quantity)
+
+ 
 if __name__ == '__main__':
     unittest.main()
